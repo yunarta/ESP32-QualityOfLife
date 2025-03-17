5c404679127a432f117f9222c0317671221c016b
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2025-03-17 16:30:12 +0800

    Initial commit

---
+++ include/ESP32QoL.h
@@ -0,0 +1,12 @@
+//
+// Created by yunarta on 3/17/25.
+//
+
+#ifndef ESP32QOL_H
+#define ESP32QOL_H
+
+#include <Arduino.h>
+
+void perform_ota_update(String url);
+
+#endif //ESP32QOL_H
---
+++ src/ESP32QoL.cpp
@@ -0,0 +1,52 @@
+//
+// Created by yunarta on 3/17/25.
+//
+
+#include "ESP32QoL.h"
+
+#include <Update.h>
+#include <HTTPClient.h>
+
+void perform_ota_update(String url) {
+    WiFiClientSecure httpClient;
+    httpClient.setInsecure();  // Use HTTPS without SSL verification (for testing)
+    HTTPClient http;
+
+    Serial.println("Starting OTA Update...");
+    http.begin(httpClient, url);
+    int httpCode = http.GET();
+
+    if (httpCode == HTTP_CODE_OK) {
+        int contentLength = http.getSize();
+        if (contentLength <= 0) {
+            Serial.println("No content received, aborting.");
+            return;
+        }
+
+        if (!Update.begin(contentLength)) {
+            Serial.println("Not enough space for OTA update.");
+            return;
+        }
+
+        WiFiClient *stream = http.getStreamPtr();
+        size_t written = 0;
+        while (written < contentLength) {
+            size_t size = stream->available();
+            if (size) {
+                uint8_t buff[128];
+                int read = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+                written += Update.write(buff, read);
+            }
+        }
+
+        if (Update.end()) {
+            Serial.println("OTA Update Successful! Restarting...");
+            ESP.restart();
+        } else {
+            Serial.println("OTA Update Failed!");
+        }
+    } else {
+        Serial.printf("HTTP Request Failed, Error: %d\n", httpCode);
+    }
+    http.end();
+}
